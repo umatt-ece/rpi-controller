aa9b787b2a7152544133c5771c6689b9df01b415
kameron <khr675@gmail.com>
2023-02-22 00:44:25 -0600

    [5] setup DataStore

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,3 @@
 from data_store import DataStore
+from live_data import LiveData
+from stored_data import StoredData
--- database/data_store.py
+++ database/data_store.py
@@ -1,18 +1,38 @@
 import redis
+from typing import Union, Any
+from database import LiveData, StoredData
 
 
 class DataStore:
 
     def __init__(self):
-        pass
+        self._live_redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._stored_redis = redis.Redis(host="localhost", port=6379, db=1)
+        # self._logger  # TODO: implement logging
 
-    def get(self, key: str):
-        pass
+    def initialize_database(self):
+        for entry in LiveData:
+            self._live_redis.set(entry.name, entry.datatype())
+        for entry in StoredData:
+            pass
+
+    def set(self, key: Union[LiveData, StoredData], value: Any):
+        if isinstance(key, LiveData):
+            return self._live_redis.set(key.name, value)
+        elif isinstance(key, StoredData):
+            return self._stored_redis.set(key.name, value)
+        else:
+            raise KeyError(f"'{key}' is not a valid parameter")
+
+    def get(self, key: Union[LiveData, StoredData]):
+        if isinstance(key, LiveData):
+            return self._live_redis.get(key.name)
+        elif isinstance(key, StoredData):
+            return self._stored_redis.get(key.name)
+        else:
+            raise KeyError(f"'{key}' is not a valid parameter")
 
     def get_many(self, keys: list):
-        pass
-
-    def set(self, key: str, value):
         pass
 
     def set_many(self, key_values: dict):
