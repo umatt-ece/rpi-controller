b17eb3ac2ea5fc2d40b4a6d4fce314c6d66c26c2
kameron <khr675@gmail.com>
2023-04-29 09:38:28 -0500

    [april 28] added frontend rebuild script (4)

--- scripts/rebuild-frontend.sh
+++ scripts/rebuild-frontend.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 # NOTE: to run this script use 'bash scripts/rebuild-frontend.sh' from ./rpi-controller
-echo building frontend...
+echo installing dependencies...
 
 cd display
 npm install
+
+echo rebuilding frontend...
+
 npm run build
+
+echo moving files to server/vue-app...
+
+rm -r ../server/vue-app/*
 mv dist/* ../server/vue-app
 
-echo build successful!
+echo rebuild finished!
+
+# If you have issues with 'git pull' after using this script you can use 'git reset --hard' to hopefully fix it
