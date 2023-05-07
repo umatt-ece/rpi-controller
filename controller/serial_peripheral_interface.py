7d47e68619cd9d61870ecf3ce559c818ff6a91dd
kameron <khr675@gmail.com>
2023-05-07 16:40:49 -0500

    [may 7, 2023] rpi testing

--- controller/serial_peripheral_interface.py
+++ controller/serial_peripheral_interface.py
@@ -1,10 +1,10 @@
 from controller import GPIOHandler
 
 
-
 class SerialPeripheralInterface():
     def __init__(self):
         self.gpio = GPIOHandler()
+        self.gpio.init_pins()
 
     def write_message(self, channel, message):
-        pass
+        print("sending message...")
