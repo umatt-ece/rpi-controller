e38fd818647093916ff18ea4898066953a2ae605
kameron <khr675@gmail.com>
2023-05-07 17:03:07 -0500

    [may 7, 2023] rpi testing

--- controller/serial_peripheral_interface.py
+++ controller/serial_peripheral_interface.py
@@ -1,12 +1,13 @@
 from controller import GPIOHandler
 from common import RpiPin as Pin
 
+
 class SerialPeripheralInterface():
     def __init__(self):
         self.gpio = GPIOHandler()
         self.gpio.init_pins()
 
-    def write_message(self, channel: Pin, message):
+    def write_message(self, channel: Pin, message: list[int]):
         print(f"sending message: {message}")
         self.gpio.set_pin(channel, 0)
         # time.sleep(0.005)
@@ -20,14 +21,21 @@ class SerialPeripheralInterface():
         self.gpio.set_pin(channel, 1)
         # time.sleep(0.005)
 
-    def read_message(self, channel: Pin, bits: int) -> list:
-        message = []
+    def read_message(self, channel: Pin, message: list[int], bits: int) -> list:
+        read = []
+        self.gpio.set_pin(channel, 0)
+        for entry in message:
+            self.gpio.set_pin(Pin.MOSI, entry)
+            self.gpio.set_pin(Pin.CLK, 1)
+            # time.sleep(0.005)
+            self.gpio.set_pin(Pin.CLK, 0)
+            # time.sleep(0.005)
         for entry in range(bits):
-            GPIO.output(p_clk, 1)
+            self.gpio.set_pin(Pin.CLK, 1)
             # time.sleep(0.005)
-            read.append(GPIO.input(p_miso))
-            GPIO.output(p_clk, 0)
+            read.append(self.gpio.read_pin(Pin.MISO))
+            self.gpio.set_pin(Pin.CLK, 0)
             # time.sleep(0.005)
-        GPIO.output(slave, 1)
+        self.gpio.set_pin(channel, 1)
         # time.sleep(0.005)
         return message
