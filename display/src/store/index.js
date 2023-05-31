c8c98c4c44da7cb03fa2bc3c4c047b2414c077b4
ZacharyDeGraeve <degraevz@myumanitoba.ca>
2023-05-31 06:13:49 -0500

    This code is a bit of a mess right now that doesn't work. But trying to access backend and update store variables. Also have all the store variables in here right now.

--- display/src/store/index.js
+++ display/src/store/index.js
@@ -9,16 +9,56 @@ const store = createStore({
   },
   state: {
     title: "Vuex Store",
-    engineTemperature: 0.0,
-    enginePower: 0,
+      /* ------ Home ------ */
+      /* Primary Info */
+      speed: liveData.state.data1,
+      torque: 1849,
+      gear: 1,  // 0: 'park', 1: 'drive', 2: 'reverse'
+      /* Secondary Info */
+      engineTemp: 182,
+      enginePower: 32,
+      engineSpeed: 194,
+      power: 13.6,
+      driveHours: 7.8,
+      systemTime: "09:36",
+      /* ------ Diagnostics ------ */
+      genericInfo: "Unknown",
+      voltage: 13.9,
+      gslPosition: 0,
+      diffLockPosition: "unlocked",
+      brakeInterlock: "off",
+      seatInterlock: "on",
+      neutralInterlock: "off",
+      //engineSpeed: 3590,
+      oilPressure: 50.0,
+      highVoltageSystem: "on",
+      electricMotorSpeed: 2000,
+      implementInterlock: "NA",
+      brakeAwayInterlock: "on",
+      throttleInterlock: "off"
   },
   getters: {
     getEngineTemp() {
-      return this.state.engineTemperature
+      return this.state.engineTemp
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    updateSpeed(state, speed) {
+      state.speed = speed;
+    }
+  },
+  actions: {
+    fetchSpeed({ commit }) {
+      fetch('/sys/get_speed')
+          .then(response => response.json())
+          .then(data => {
+            commit('updateSpeed', data);
+          })
+          .catch(error => {
+            console.error('Error fetching speed:', error);
+          });
+    },
+  },
 })
 
 export default store;
