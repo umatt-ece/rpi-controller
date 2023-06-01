c75661853da6818be0f1c202cd56924f62d79179
kameron <khr675@gmail.com>
2023-06-01 09:56:34 -0500

    [May 31, 2023] rpi testing

--- controller/expander.py
+++ controller/expander.py
@@ -16,10 +16,10 @@ class Expander:
         for pin in [Pin.GPIO1_SELECT, Pin.GPIO2_SELECT, Pin.GPIO3_SELECT, Pin.GPIO4_SELECT]:
             self._gpio.set(pin, 1)
 
-        self.init_gpio(Pin.GPIO1_SELECT, SpiByte.ALL_INPUT.value, SpiByte.ALL_OUTPUT.value)
-        self.init_gpio(Pin.GPIO2_SELECT, SpiByte.ALL_INPUT.value, SpiByte.ALL_INPUT.value)
-        self.init_gpio(Pin.GPIO3_SELECT, SpiByte.ALL_OUTPUT.value, SpiByte.ALL_INPUT.value)
-        self.init_gpio(Pin.GPIO4_SELECT, [0, 0, 1, 1, 1, 1, 1, 1], SpiByte.ALL_INPUT.value)
+        self.init_gpio(Pin.GPIO1_SELECT, SpiByte.ALL_OUTPUT.value, SpiByte.ALL_INPUT.value)  # peripherals 'A' and 'B'
+        # self.init_gpio(Pin.GPIO2_SELECT, SpiByte.ALL_INPUT.value, SpiByte.ALL_INPUT.value)
+        self.init_gpio(Pin.GPIO3_SELECT, SpiByte.ALL_OUTPUT.value, SpiByte.ALL_OUTPUT.value)  # sevcon on 'A'
+        self.init_gpio(Pin.GPIO4_SELECT, SpiByte.ALL_OUTPUT.value, [0, 0, 1, 1, 1, 1, 1, 1])  # peripherals 'A' and 'B'
 
     def init_gpio(self, select: Pin, config_a: list[int], config_b: list[int]):
         self._spi.write(select, SpiByte.GPIO_OPCODE_WRITE.value + SpiByte.GPIO_SELECT_IOCON.value + SpiByte.ALL_OUTPUT.value)
