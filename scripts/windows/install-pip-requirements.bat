a64fc76f52360ff30f5122cd2f22a766dfcc9405
umatt <umattinfo@gmail.com>
2024-01-15 12:40:32 -0600

    [2024/01/15] Added pip install script for windows

--- scripts/windows/install-pip-requirements.bat
+++ scripts/windows/install-pip-requirements.bat
@@ -1,1 +1,7 @@
 @echo off
+
+pip install -r common/requirements.txt
+pip install -r database/requirements.txt
+pip install -r hardware/requirements.txt
+pip install -r logic/requirements.txt
+pip install -r server/requirements.txt
